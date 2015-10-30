b5853701696b222df9a7eb78bd1f3a8c7f8d95fc
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2015-10-30 13:56:26 -0500

    try to get device-specific node id and fall back to random as backup

--- scripts/waggle_first_boot.sh
+++ scripts/waggle_first_boot.sh
@@ -69,29 +69,40 @@ do_start()
 	  return 2
 	fi
 
-	### hostname
-	export UNIQUE="undefined"
+	### Node ID
+	export NODE_ID=""
+	
+	# try MAC address (some older models do not have unique MAC addresses)
 	if [ ${ODROIDMODEL}_ == "ODROIDC_" ] ; then
-	  export UNIQUE=`ifconfig eth0 | head -n 1 | grep -o "[[:xdigit:]:]\{17\}" | sed 's/://g'`
-	  if [ ! ${#UNIQUE} -ge 12 ]; then
-	    echo "error: could not extract MAC address"
-	    return 2
-	  fi
-	else
-	  # use serial number from SD-card
-	  # some devices do not have a unique MAC address, they could use this code
-	  export CID_FILE="/sys/block/mmcblk0/device/cid"
-	  if [ ! -e ${CID_FILE} ] ; then 
-	    echo "error: File not found: ${CID_FILE}" 
-	    return 2 
-	  fi
-	  export UNIQUE=`python -c "cid = '$(cat ${CID_FILE})' ; len=len(cid) ; mid=cid[:2] ; psn=cid[-14:-6] ; print mid+'_'+psn"`
-	  if [ ! ${#UNIQUE} -ge 11 ]; then
-	    echo "error: could not create unique identifier from SD-card serial number"
-	    return 2
+	  export MACADDRESS=`ifconfig eth0 | head -n 1 | grep -o "[[:xdigit:]:]\{17\}" | sed 's/://g'`
+	  if [ ! ${#MACADDRESS} -ge 12 ]; then
+	    echo "warning: could not extract MAC address"
+	  else
+	    NODE_ID=${MACADDRESS}  
 	  fi
 	fi
-	echo waggle_${UNIQUE} > /etc/hostname
+	
+	# try memory card serial number
+	export CID_FILE="/sys/block/mmcblk0/device/cid"
+	if [ "${NODE_ID}x" == "x" ] && [ -e ${CID_FILE} ]; then
+	  # use serial number from SD-card
+	  # some devices do not have a unique MAC address, they could use this code
+	 
+	  export SERIAL_ID=`python -c "cid = '$(cat ${CID_FILE})' ; len=len(cid) ; mid=cid[:2] ; psn=cid[-14:-6] ; print mid+'_'+psn"`
+	  if [ ! ${#SERIAL_ID} -ge 11 ]; then
+	    echo "warning: could not create unique identifier from SD-card serial number"
+	  else
+	    NODE_ID=${SERIAL_ID} 
+	  fi
+	fi
+	
+	# try random number
+	if [ "${NODE_ID}x" == "x" ] ; then
+	  NODE_ID="random_${RANDOM}"
+	fi
+	
+	echo waggle_${NODE_ID} > /etc/waggle/node_id
+	echo waggle_${NODE_ID} > /etc/hostname
 
 	# new host keys
 	# Not needed here because they will be recreated by the /etc/rc.local script by default.
