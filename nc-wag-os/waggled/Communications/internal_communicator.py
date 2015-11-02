a4dee86c566eb946037822e244913db4e9320ce9
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2015-11-01 21:31:28 -0600

    fix

--- nc-wag-os/waggled/Communications/internal_communicator.py
+++ nc-wag-os/waggled/Communications/internal_communicator.py
@@ -187,7 +187,7 @@ class push_server(Process):
         try:  
           server.bind((HOST,PORT))
         except socket.error as msg:
-          sys.stdout.write( "(server.bind) Socket Error: %s (%s, %s)\n" % msg, HOST, PORT )
+          sys.stdout.write( "(server.bind) Socket Error: %s (%s, %s)\n" % (msg, HOST, PORT) )
           return 1
         try:
           server.listen(5) #supports up to 5 threads, one for each GN
