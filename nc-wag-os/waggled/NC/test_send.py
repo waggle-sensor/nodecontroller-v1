2ed727e410ede1e534a8392c5d968760f6e0c9fc
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2015-11-02 15:10:13 -0600

    correct timestamp

--- nc-wag-os/waggled/NC/test_send.py
+++ nc-wag-os/waggled/NC/test_send.py
@@ -5,10 +5,22 @@ from utilities import packetmaker
 sys.path.append('../Communications/')
 from internal_communicator import send
 import time
+
+
+def unix_time(dt):
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    delta = dt - epoch
+    return delta.total_seconds()
+
+def unix_time_millis(dt):
+    return long(unix_time(dt) * 1000.0)
+    
+
+
 count = 0
 while 1:
     tempC = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
-    sendData=['sensor1', int(time.time()), ['Temperature']  , ['i'], [tempC], ['Celsius'], ['count='+str(count)]]
+    sendData=['sensor1', int(unix_time_millis(datetime.datetime.now())), ['Temperature']  , ['i'], [tempC], ['Celsius'], ['count='+str(count)]]
     print 'Sending data: ',sendData
     #packs and sends the data
     packet = packetmaker.make_data_packet(sendData)
