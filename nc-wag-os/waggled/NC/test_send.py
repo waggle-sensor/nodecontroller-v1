cb1052718ad6a2e8fe38ad7cb3cd8f7d60ffa1b8
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2015-11-02 15:30:43 -0600

    sensor name

--- nc-wag-os/waggled/NC/test_send.py
+++ nc-wag-os/waggled/NC/test_send.py
@@ -20,14 +20,14 @@ def unix_time_millis(dt):
 count = 0
 while 1:
     tempC = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
-    sendData=['sensor1', int(unix_time_millis(datetime.datetime.now())), ['Temperature']  , ['i'], [tempC], ['Celsius'], ['count='+str(count)]]
+    sendData=['CPU temperature', int(unix_time_millis(datetime.datetime.now())), ['Temperature']  , ['i'], [tempC], ['Celsius'], ['count='+str(count)]]
     print 'Sending data: ',sendData
     #packs and sends the data
     packet = packetmaker.make_data_packet(sendData)
     for pack in packet:
         send(pack)
-    #send a packet every second
-    time.sleep(1)
+    #send a packet every 10 seconds
+    time.sleep(10)
     count = count + 1
 
 
