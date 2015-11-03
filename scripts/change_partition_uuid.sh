dfeb8e3471b17138bea39062c6c4f9283f36230e
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-11-02 22:28:47 -0600

    take argument

--- scripts/change_partition_uuid.sh
+++ scripts/change_partition_uuid.sh
@@ -5,7 +5,52 @@ set -x
 # Do not try to change the UUID's of partitions of eMMC or SD-cards plugged into their native odroid slot. 
 # If you want to use an odroid for this, connect the memory via USB-adapter (e.g. device /dev/sda ).
 
-export OTHER_DEVICE="/dev/sda"
+
+if [ $# -eq 0 ] ; then
+  echo "usage: $0 <device>"
+  exit 1
+fi
+
+
+export OTHER_DEVICE="$1"
+
+if [ ! $(lsblk -o KNAME,TYPE ${OTHER_DEVICE} | grep -c disk) -eq 1 ] ; then
+  echo "device $1 not found."
+  exit 1
+fi
+
+export OTHER_DEVICE=`basename ${OTHER_DEVICE}`
+
+echo "OTHER_DEVICE: /dev/${OTHER_DEVICE}"
+
+
+
+function dev_suffix {
+
+  if [[ $1 =~ ^"/dev/sd" ]] ; then
+    echo ""
+    return 0
+  fi
+  if [[ $1 =~ ^"/dev/mmcblk" ]] ; then
+    echo "p"
+    return 0
+  fi
+  if [[ $1 =~ ^"/dev/disk" ]] ; then
+	echo "s"
+	return 0
+  fi
+
+  echo "unknown"
+  return 1
+}
+
+export OTHER_DEV_SUFFIX=`dev_suffix "/dev/${OTHER_DEVICE}"`
+if [ "${OTHER_DEV_SUFFIX}_" == "unknown_" ] ; then
+  exit 1
+fi
+
+echo "OTHER_DEV_SUFFIX: ${OTHER_DEV_SUFFIX}"
+
 
 
 if ! $(hash uuidgen 2>/dev/null) ; then
@@ -23,20 +68,20 @@ export NEWUUID_1_HEX=`echo -n "${NEWUUID_1}" | od -t x2 | head -n 1 | sed "s/^00
 
 #unmount the other partitions
 set +e
-if [ $(df -h | grep -c ${OTHER_DEVICE}1 ) == 1 ] ; then 
-  while ! $(umount ${OTHER_DEVICE}1) ; do sleep 3 ; done
+if [ $(df -h | grep -c ${OTHER_DEVICE}${OTHER_DEV_SUFFIX}1 ) == 1 ] ; then
+  while ! $(umount ${OTHER_DEVICE}${OTHER_DEV_SUFFIX}1) ; do sleep 3 ; done
 fi
-if [ $(df -h | grep -c ${OTHER_DEVICE}2 ) == 1 ] ; then 
-  while ! $(umount ${OTHER_DEVICE}2) ; do sleep 3 ; done
+if [ $(df -h | grep -c ${OTHER_DEVICE}${OTHER_DEV_SUFFIX}2 ) == 1 ] ; then
+  while ! $(umount ${OTHER_DEVICE}${OTHER_DEV_SUFFIX}2) ; do sleep 3 ; done
 fi
 set -e
 sleep 2
 
 ###  change UUID on other devices
-tune2fs -U ${NEWUUID_2} ${OTHER_DEVICE}2
+tune2fs -U ${NEWUUID_2} ${OTHER_DEVICE}${OTHER_DEV_SUFFIX}2
 
 # the boot partition uses FAT16. To change the UUID we use dd
-echo -n "${NEWUUID_1}" | dd of=${OTHER_DEVICE}1 bs=1 seek=39 count=4
+echo -n "${NEWUUID_1}" | dd of=${OTHER_DEVICE}${OTHER_DEV_SUFFIX}1 bs=1 seek=39 count=4
 # FAT16: (seek=39 count=4)
 # FAT32: (seek=67 count=4)
 # NTFS: (seek=72 count=8)
@@ -44,7 +89,7 @@ echo -n "${NEWUUID_1}" | dd of=${OTHER_DEVICE}1 bs=1 seek=39 count=4
 
 # fstab on other device
 mkdir -p /media/other/
-mount ${OTHER_DEVICE}2 /media/other/
+mount ${OTHER_DEVICE}${OTHER_DEV_SUFFIX}2 /media/other/
 sed -i.bak -e "s/[^ ]*[ $'\t']*\/[ $'\t']/UUID=${NEWUUID_2}\t\/\t/" \
            -e "s/[^ ]*[ $'\t']*\/media\/boot[ $'\t']/UUID=${NEWUUID_1_HEX}\t\/media\/boot\t/" /media/other/etc/fstab
 # verify: diff /media/other/etc/fstab /media/other/etc/fstab.bak
@@ -54,7 +99,7 @@ set -e
 
 #boot.scr on the boot partition of the other device
 mkdir -p /media/other_boot/
-mount ${OTHER_DEVICE}1 /media/other_boot/
+mount ${OTHER_DEVICE}${OTHER_DEV_SUFFIX}1 /media/other_boot/
 for file in boot.txt boot.ini ; do
   if [ -e /media/other_boot/${file} ] ; then
     sed -i.bak -e "s/root\=[^ ]*/root=UUID=${NEWUUID_2}/" /media/other_boot/${file}
