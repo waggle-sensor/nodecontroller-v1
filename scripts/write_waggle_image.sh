0642be0e8097e05048aacd2a1c97a4619c174f08
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-11-05 13:17:57 -0600

    more robust

--- scripts/write_waggle_image.sh
+++ scripts/write_waggle_image.sh
@@ -2,9 +2,12 @@
 set -x
 IMAGE="waggle-odroid-c1-20151105.img"
 USB_NAME="Transcend"
-SD_DISK=""
+DEVICE_NAME=""
 
-### Use 
+### Use this script at your own risk ###
+
+
+# OSX: brew install pv
 
 
 if [ ! -e ${IMAGE} ] ; then
@@ -17,21 +20,36 @@ if [[ "$unamestr" == 'Darwin' ]]; then
 
 
   while [ 1 ] ; do
-    SD_DISK=$(system_profiler SPUSBDataType | grep -A 20  "${USB_NAME}"  | grep "BSD Name" | cut -d ':' -f 2 | tr -d ' ')
-    if [ "${SD_DISK}_" != "_" ] ; then
-  
+    set +x
+    echo "waiting for memory device..."
+    while [ "${DEVICE_NAME}_" == "_" ] ; do
       sleep 2
-      export DEVICE_NAME=${SD_DISK}
-      diskutil unmountDisk /dev/${DEVICE_NAME}
+      DEVICE_NAME=$(system_profiler SPUSBDataType | grep -A 20  "${USB_NAME}"  | grep -m 1 "BSD Name" | cut -d ':' -f 2 | tr -d ' ' | tr -d '\n')
+
+      if [ "${DEVICE_NAME}_" != "_" ] ;then
+        if [ ! -e ${DEVICE_NAME} ] ; then
+          DEVICE_NAME=""
+        fi
+      fi
+    done
+
+    if [ "${DEVICE_NAME}_" != "_" ] && [ -e /dev/r${DEVICE_NAME} ] ; then
+      echo "found device: ${SD_DISK}"
       sleep 2
-      dd if=${IMAGE} of=/dev/r${DEVICE_NAME} bs=1m
+      
+      set -x
+      if [ $(diskutil list | grep -c "^/dev/${DEVICE_NAME}") -eq 1 ] ; then      
+        diskutil unmountDisk /dev/${DEVICE_NAME}
+        sleep 2
+      fi
+      #dd if=${IMAGE} of=/dev/r${DEVICE_NAME} bs=1m
+      pv -per --width 80 -f ${IMAGE} | dd of=/dev/r${DEVICE_NAME} bs=1m
       sleep 2
       sync
       diskutil eject /dev/r${DEVICE_NAME}
-
+      set +x
     fi
-      echo "waiting for memory device..."
-      sleep 2
+    sleep 2  
 
   done
 
