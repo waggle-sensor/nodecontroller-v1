ab39361ae814c50ad2dd130e95fee00a4721df0c
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2015-11-05 12:34:25 -0600

    fix indent

--- scripts/write_waggle_image.sh
+++ scripts/write_waggle_image.sh
@@ -4,6 +4,9 @@ IMAGE="waggle-odroid-c1-20151105.img"
 USB_NAME="Transcend"
 SD_DISK=""
 
+### Use 
+
+
 if [ ! -e ${IMAGE} ] ; then
   echo "Error: Image file ${IMAGE} not found"
   exit 1
@@ -13,24 +16,24 @@ unamestr=`uname`
 if [[ "$unamestr" == 'Darwin' ]]; then
 
 
-while [ 1 ] ; do
+  while [ 1 ] ; do
     SD_DISK=$(system_profiler SPUSBDataType | grep -A 20  "${USB_NAME}"  | grep "BSD Name" | cut -d ':' -f 2 | tr -d ' ')
-  if [ "${SD_DISK}_" != "_" ] ; then
+    if [ "${SD_DISK}_" != "_" ] ; then
   
-    sleep 2
-    export DEVICE_NAME=${SD_DISK}
-    diskutil unmountDisk /dev/${DEVICE_NAME}
-    sleep 2
-    dd if=${IMAGE} of=/dev/r${DEVICE_NAME} bs=1m
-    sleep 2
-    sync
-    diskutil eject /dev/r${DEVICE_NAME}
+      sleep 2
+      export DEVICE_NAME=${SD_DISK}
+      diskutil unmountDisk /dev/${DEVICE_NAME}
+      sleep 2
+      dd if=${IMAGE} of=/dev/r${DEVICE_NAME} bs=1m
+      sleep 2
+      sync
+      diskutil eject /dev/r${DEVICE_NAME}
 
-  fi
-    echo "waiting for memory device..."
-    sleep 2
+    fi
+      echo "waiting for memory device..."
+      sleep 2
 
-done
+  done
 
 else
   echo "TODO"
