d362694090fbd65f195b2f7b884932f4b8baa9de
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-12-09 13:15:19 -0600

    include other services

--- scripts/nodecontroller.sh
+++ scripts/nodecontroller.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
 echo
-echo -n "Data Cache:    "
+echo -n "Data Cache:                   "
 /etc/init.d/data_cache.sh status
 
 echo
-echo -n "Communication: "
+echo -n "Communication:                "
 /etc/init.d/communications.sh status
 
 echo
-echo -n "Sensor:        "
-/etc/init.d/start_sensor.sh status
+echo -n "core sense:                   "
+/etc/init.d/waggle_core_sense.sh status
+
+if [ -e /etc/init.d/WagMan_start.sh ] ; then
+  echo
+  echo -n "WagMan:                       "
+  /etc/init.d/WagMan_start.sh status
+fi
+ 
+if [ -e /etc/init.d/heartbeat_setup_start.sh ] ; then
+  echo
+  echo -n "heartbeat_setup_start.sh:     "
+  /etc/init.d/heartbeat_setup_start.sh status
+fi
+
+if [ -e /etc/init.d/heartbeat_start.sh ] ; then
+  echo
+  echo -n "heartbeat_start.sh:           "
+  /etc/init.d/heartbeat_start.sh status
+fi
+
+
+
+
+
 
 echo
