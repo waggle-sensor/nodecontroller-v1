72b3d5c4835d12dde0387e350297be992c3c2a9b
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2016-01-19 16:04:17 -0600

    use new scheme

--- nc-wag-os/waggled/NC/sensor.py
+++ nc-wag-os/waggled/NC/sensor.py
@@ -169,7 +169,11 @@ try:
                                     pass
                         for all in range(len(sensorReading_bucket)):
                             if (sensorReading_bucket[all] <> [[],[],[],[],[]]):
-                                sendData=[sensor_names[all],int(unix_time_millis(datetime.datetime.now())),sensorReading_bucket[all][0],sensorReading_bucket[all][1],sensorReading_bucket[all][2],sensorReading_bucket[all][3],sensorReading_bucket[all][4]]
+                                timestamp_utc = datetime.datetime.utcnow()
+                                timestamp_date = timestamp_utc.date()
+                                timestamp_epoch =  int(float(timestamp_utc.strftime("%s.%f"))) * 1000
+                                sendData=[str(timestamp_date), 'env_sense', '1', 'default', str(timestamp_epoch), sensorReading_bucket[all][0], sensorReading_bucket[all][2], [] ]
+                                #sendData=[str(timestamp_date), sensor_names[all],int(unix_time_millis(datetime.datetime.now())),sensorReading_bucket[all][0],sensorReading_bucket[all][1],sensorReading_bucket[all][2],sensorReading_bucket[all][3],sensorReading_bucket[all][4]]
                                 print 'Sending data: ',sendData
                                 #packs and sends the data
                                 packet = packetmaker.make_data_packet(sendData)
