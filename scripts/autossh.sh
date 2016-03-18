5e8a9d2df551962e1a92073855c364f896d15dca
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-03-18 16:00:43 -0500

    host key check disabled

--- scripts/autossh.sh
+++ scripts/autossh.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-autossh -i /usr/lib/waggle/SSL/node/key.pem -M 0 -gNC -o "ServerAliveInterval 5" -o "ServerAliveCountMax 3" -R `cat /etc/waggle/reverse_ssh_port`:localhost:22 root@beehive1.mcs.anl.gov  -p 20022
+
+# TODO! remove these options: -o "StrictHostKeyChecking no" -o " UserKnownHostsFile=/dev/null"
+
+autossh -i /usr/lib/waggle/SSL/node/key.pem -M 0 -gNC -o "ServerAliveInterval 5" -o "ServerAliveCountMax 3" -R `cat /etc/waggle/reverse_ssh_port`:localhost:22 -o "StrictHostKeyChecking no" -o " UserKnownHostsFile=/dev/null" root@beehive1.mcs.anl.gov  -p 20022
