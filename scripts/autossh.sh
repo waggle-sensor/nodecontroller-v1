8fa6302307bf1c380d024f91b984af4e9ac527ce
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-03-23 15:03:41 -0500

    more verbose

--- scripts/autossh.sh
+++ scripts/autossh.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+set -x
 
-# TODO! remove these options: -o "StrictHostKeyChecking no" -o " UserKnownHostsFile=/dev/null"
+# TODO! remove these options: -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null"
 
-autossh -i /usr/lib/waggle/SSL/node/key.pem -M 0 -gNC -o "ServerAliveInterval 5" -o "ServerAliveCountMax 3" -R `cat /etc/waggle/reverse_ssh_port`:localhost:22 -o "StrictHostKeyChecking no" -o " UserKnownHostsFile=/dev/null" root@beehive1.mcs.anl.gov  -p 20022
+autossh -i /usr/lib/waggle/SSL/node/key.pem -M 0 -gNC -o "ServerAliveInterval 5" -o "ServerAliveCountMax 3" -R `cat /etc/waggle/reverse_ssh_port`:localhost:22 -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" root@beehive1.mcs.anl.gov  -p 20022
