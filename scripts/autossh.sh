4356ec510e0bd33de5985e5e33ba86ed4e039658
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-06-15 16:00:43 -0500

    Update autossh.sh
    added some respawn insurance.

--- scripts/autossh.sh
+++ scripts/autossh.sh
@@ -5,3 +5,7 @@ set -x
 # TODO! remove these options: -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null"
 
 autossh -i /usr/lib/waggle/SSL/node/key.pem -M 0 -gNC -o "ServerAliveInterval 5" -o "ServerAliveCountMax 3" -R `cat /etc/waggle/reverse_ssh_port`:localhost:22 -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" root@beehive1.mcs.anl.gov  -p 20022
+
+# For us, autossh should never exit unless there's a problem...however, it's possible that it doesn't
+# exit with an error code needed to have upstart respawn it. This is to correct that possible behavior.
+exit 1
