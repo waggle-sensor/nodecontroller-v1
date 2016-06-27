aac7bf985e7054fb567a9525df4185b1a5ddaca1
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-06-27 14:19:04 -0500

    do not use encode

--- wagman/wagman-server.py
+++ wagman/wagman-server.py
@@ -67,7 +67,8 @@ if __name__ == "__main__":
                     print("Received request: ", message)
     
                     try:
-                        serial.write(message.encode('ascii'))
+                        #serial.write(message.encode('ascii'))
+                        serial.write(message)
                         serial.write(b'\n')
                     except Exception as e:
                         server_socket.send_string("error (serial.write): %s" % str(e))
