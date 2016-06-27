42d901ce1677afb26db2d6f48a32b7a3758b9430
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-06-27 14:19:38 -0500

    simple linebreak

--- wagman/wagman-server.py
+++ wagman/wagman-server.py
@@ -69,7 +69,8 @@ if __name__ == "__main__":
                     try:
                         #serial.write(message.encode('ascii'))
                         serial.write(message)
-                        serial.write(b'\n')
+                        #serial.write(b'\n')
+                        serial.write('\n')
                     except Exception as e:
                         server_socket.send_string("error (serial.write): %s" % str(e))
                         raise Exception('Could not write to %s: %s' % (wagman_device, str(e)))
