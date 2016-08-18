46939f6aa42a37ee68e37704fe93bf77d4caf3a2
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-08-18 14:33:05 -0500

    collects epoch times and print max

--- utils/sync-date.py
+++ utils/sync-date.py
@@ -4,32 +4,33 @@ from datetime import datetime
 
 
 def get_system_date():
-    output = subprocess.check_output(['date', '+%Y %m %d %H %M %S']).decode()
-    year, month, day, hour, minute, second = map(int, output.split())
-    return datetime(year, month, day, hour, minute, second)
+    output = subprocess.check_output(['date', '+%s']).decode()
+    return int(output)
 
 
 def get_wagman_date():
-    output = subprocess.check_output(['python3', 'wagman_client.py', 'date'])
-    year, month, day, hour, minute, second = map(int, output.split())
-    return datetime(year, month, day, hour, minute, second)
+    output = subprocess.check_output(['wagman-client', 'date']).decode().strip()
+    return int(datetime.strptime(output, '%Y %m %d %H %M %S').strftime('%s'))
+
+
+def get_guest_node_date():
+    output = subprocess.check_output(['ssh', '-i', '/usr/lib/waggle/SSL/guest/id_rsa_waggle_aot_guest_node', 'waggle@10.31.81.51', 'date', '+%s']).decode()
+    return int(output)
 
 
 def get_beehive_date():
+    r = requests.get('http://beehive1.mcs.anl.gov/api/1/epoch')
+    assert r.status_code == 200
+    response = r.json()
+    return int(response['epoch'])
+
+
+dates = []
+
+for get_date in [get_system_date, get_wagman_date, get_beehive_date, get_guest_node_date]:
     try:
-        r = requests.get('http://beehive1.mcs.anl.gov/api/1/epoch')
-        assert r.status_code == 200
-        response = r.json()
-        timestamp = int(response['epoch'])
-        return datetime.fromtimestamp(timestamp)
+        dates.append(get_date())
     except:
-        return None
+        pass
 
-
-beehive_date = get_beehive_date()
-
-if beehive_date:
-    print('choosing beehive', beehive_date)
-else:
-    latest_date = max(get_wagman_date(), get_system_date())
-    print(latest_date)
+print('max date', max(dates))
