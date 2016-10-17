6d29f3112b4272b241c0727bbfd7ae57048121b8
Peter Lane <lane@uchicago.edu>
2016-10-17 15:28:12 -0500

    add rabbitmq user to beehive group

--- scripts/configure-system.sh
+++ scripts/configure-system.sh
@@ -31,7 +31,9 @@ echo >> /home/waggle/.ssh/authorized_keys
 echo "from=\"10.31.81.5?\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4ohQv1Qksg2sLIqpvjJuZEsIkeLfbPusEaJQerRCqI71g8hwBkED3BBv5FehLcezTg+cFJFhf2vBGV5SbV0NzbouIM+n0lAr6+Ei/XYjO0B1juDm6cUmloD4HSzQWv+cSyNmb7aXjup7V0GP1DZH3zlmvwguhMUTDrWxQxDpoV28m72aZ4qPH7VmQIeN/JG3BF9b9F8P4myOPGuk5XTjY1rVG+1Tm2mxw0L3WuL6w3DsiUrvlXsGE72KcyFBDiFqOHIdnIYWXDLZz61KXctVLPVLMevwU0YyWg70F9pb0d2LZt7Ztp9GxXBRj5WnU9IClaRh58RsYGhPjdfGuoC3P AoT_guest_node_key" >> /home/waggle/.ssh/authorized_keys
 echo >> /home/waggle/.ssh/authorized_keys
 
+# add rabbitmq to the beehive group so that it can read the node credentials
 addgroup beehive
+usermod -aG beehive rabbitmq
 
 # Setup RabbitMQ config files.
 cp -r /usr/lib/waggle/nodecontroller/etc/rabbitmq /etc
