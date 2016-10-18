6b2dfba472071c6030ff8702f21711181d6f33ef
Raj Sankaran <rajesh@anl.gov>
2016-10-18 14:31:22 -0500

    Change toward managing ATT modem as a systemd service.

--- device_rules/wwan_modems/wvwaggle.sh
+++ device_rules/wwan_modems/wvwaggle.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #This goes in /usr/bin/wvwaggle.sh, and make the file executable with chmod +x /usr/bin/wvwaggle.sh
 #Let us give the modem time to settle.
-sleep 7
+
 rm /dev/attwwan
-sleep 2
-ln -s  $(ls /dev/attwwan[0-9] | sort | head -1) /dev/attwwan
-sleep 2
-wvdial
+sleep 1
+ln -s $(ls /dev/attwwan[0-9] | sort | head -1) /dev/attwwan
+
+
+
 
