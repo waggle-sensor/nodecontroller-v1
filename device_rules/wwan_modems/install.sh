5954603af23e535e1d56a276f34dcf4989598134
Peter Lane <lane@uchicago.edu>
2016-10-18 15:44:09 -0500

    install modem udev rules and systemd service

--- device_rules/wwan_modems/install.sh
+++ device_rules/wwan_modems/install.sh
@@ -9,5 +9,8 @@ rm -f /usr/bin/wvwaggle.sh
 cp ./wvwaggle.sh /usr/bin/
 chmod +x /usr/bin/wvwaggle.sh
 
+rm -f /etc/udev/rules.d/40-usb_modeswitch.rules
+cp 40-usb_modeswitch.rules /etc/udev/rules.d/
+
 set +x
 echo "run: udevadm control --reload-rules"
