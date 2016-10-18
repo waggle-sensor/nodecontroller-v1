07b01527a01f7fc43c6a04d80ed452914f11d0fc
Peter Lane <lane@uchicago.edu>
2016-10-18 12:42:54 -0500

    shell magic

--- scripts/configure-rabbitmq.sh
+++ scripts/configure-rabbitmq.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-/usr/lib/waggle/core/scripts/detect_mac_address.sh
+. /usr/lib/waggle/core/scripts/detect_mac_address.sh
 sed -i -e "s/%NODE_ID%/$NODE_ID/" /etc/rabbitmq/rabbitmq.config
