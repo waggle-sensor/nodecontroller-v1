76ae2bb3ae4272c12d691dca9f4bf1611076e922
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-10-20 12:28:12 -0500

    a little more cleanup

--- scripts/log-wagman.py
+++ scripts/log-wagman.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 import subprocess
-import time
 import logging
 import waggle.logging
 
 
 logger = waggle.logging.getLogger('wagman')
 logger.setLevel(logging.DEBUG)
+
+
+def wagman_output(args):
+    command = 'wagman-client {}'.format(args)
+    return subprocess.check_output(command, shell=True).decode().strip()
+
+
+def catlines(s):
+    return ' '.join(s.strip().split())
 
 
 template = '''
@@ -21,20 +29,10 @@ fails={fails}
 media={media}
 '''.strip()
 
-
-def wagman_output(args):
-    command = 'wagman-client {}'.format(args)
-    return subprocess.check_output(command, shell=True).decode().strip()
-
-
-def catlines(s):
-    return ' '.join(s.strip().split())
-
-
-results = {}
-
 for attempt in range(10):
     try:
+        results = {}
+
         results['id'] = wagman_output('id').lower()
         results['version'] = catlines(wagman_output('ver'))
         results['uptime'] = wagman_output('up')
@@ -45,9 +43,9 @@ for attempt in range(10):
         results['fails'] = catlines(wagman_output('fc'))
         results['media'] = ' '.join([wagman_output('bs 0'),
                                      wagman_output('bs 1')])
+
+        logger.info(template.format(**results))
+        break
     except Exception as e:
         print(e)
         continue
-    else:
-        logger.info(template.format(**results))
-        break
