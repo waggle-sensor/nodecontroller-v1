2b9183a7bab0a90adb4c2039725b62f5f069a344
Peter Lane <lane@uchicago.edu>
2016-11-04 03:08:55 -0500

    finished adding untested tests

--- scripts/test_node.sh
+++ scripts/test_node.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-print_result()
-  test_description=$1
-  result=$2
-  send_user "\n"
-  if { $result == 0 } {
-    echo "\[0;30;32m\[PASS\]\[0;30;37m $test_description\n"
-  } else {
-    echo "\[0;30;31m\[FAIL\]\[0;30;37m $test_description\n"
-  }
+set +e
+
+print_result() {
+  local test_description=$1
+  local result=$2
+  if [ $result == 0 ]; then
+    echo "[0;30;32m[PASS][0;30;37m $test_description"
+  else
+    echo "[0;30;31m[FAIL][0;30;37m $test_description"
+  fi
 }
 
 shadow='root:$6$D3j0Te22$md6NULvJPliwvAhK2BlL96XCsJ0KdTnPqNdufDWgyU5k6Nc3M88qO64WCKKTLZry1GgKhGE95L5ZA1i2VFQGn.:17079:0:99999:7:::'
-fgrep $shadow /etc/shadow > /dev/null
+fgrep $shadow /etc/shadow
 print_result "AoT Root Password Set" $?
 
-keys=("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsYPMSrC6k33vqzulXSx8141ThfNKXiyFxwNxnudLCa0NuE1SZTMad2ottHIgA9ZawcSWOVkAlwkvufh4gjA8LVZYAVGYHHfU/+MyxhK0InI8+FHOPKAnpno1wsTRxU92xYAYIwAz0tFmhhIgnraBfkJAVKrdezE/9P6EmtKCiJs9At8FjpQPUamuXOy9/yyFOxb8DuDfYepr1M0u1vn8nTGjXUrj7BZ45VJq33nNIVu8ScEdCN1b6PlCzLVylRWnt8+A99VHwtVwt2vHmCZhMJa3XE7GqoFocpp8TxbxsnzSuEGMs3QzwR9vHZT9ICq6O8C1YOG6JSxuXupUUrHgd AoT_key" \
-      "command=\"/bin/date\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCedz4oU6YdvFjbWiJTpJiREplTizAk2s2dH0/aBMLmslSXzMXCgAh0EZOjsA3CW+P2SIn3NY8Hx3DmMR9+a1ISd3OcBcH/5F48pejK1MBtdLOnai64JmI80exT3CR34m3wXpmFbbzQ5jrtGFb63q/n89iVDb+BwY4ctrBn+J7BPEJbhh/aepoUNSG5yICWtjC0q8mDhHzr+40rYsxPXjp9HTaEzgLu+fNhJ0rK+4891Lr08MTud2n8TEntjBRlWQUciGrPn1w3jzIz+q2JdJ35a/MgLg6aRSQOMg6AdanZH2XBTqHbaeYOWrMhmDTjC/Pw9Jczl7S+wr0648bzXz2T AoT_key_test" \
-      "from=\"10.31.81.5?\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4ohQv1Qksg2sLIqpvjJuZEsIkeLfbPusEaJQerRCqI71g8hwBkED3BBv5FehLcezTg+cFJFhf2vBGV5SbV0NzbouIM+n0lAr6+Ei/XYjO0B1juDm6cUmloD4HSzQWv+cSyNmb7aXjup7V0GP1DZH3zlmvwguhMUTDrWxQxDpoV28m72aZ4qPH7VmQIeN/JG3BF9b9F8P4myOPGuk5XTjY1rVG+1Tm2mxw0L3WuL6w3DsiUrvlXsGE72KcyFBDiFqOHIdnIYWXDLZz61KXctVLPVLMevwU0YyWg70F9pb0d2LZt7Ztp9GxXBRj5WnU9IClaRh58RsYGhPjdfGuoC3P AoT_guest_node_key")
-key_names=("AoT Key" "AoT Test Key" "AoT Guest Key")
-for i in $(seq 1 ${#keys[@]}); do
+keys=('ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsYPMSrC6k33vqzulXSx8141ThfNKXiyFxwNxnudLCa0NuE1SZTMad2ottHIgA9ZawcSWOVkAlwkvufh4gjA8LVZYAVGYHHfU/+MyxhK0InI8+FHOPKAnpno1wsTRxU92xYAYIwAz0tFmhhIgnraBfkJAVKrdezE/9P6EmtKCiJs9At8FjpQPUamuXOy9/yyFOxb8DuDfYepr1M0u1vn8nTGjXUrj7BZ45VJq33nNIVu8ScEdCN1b6PlCzLVylRWnt8+A99VHwtVwt2vHmCZhMJa3XE7GqoFocpp8TxbxsnzSuEGMs3QzwR9vHZT9ICq6O8C1YOG6JSxuXupUUrHgd AoT_key' \
+      'command="/bin/date" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCedz4oU6YdvFjbWiJTpJiREplTizAk2s2dH0/aBMLmslSXzMXCgAh0EZOjsA3CW+P2SIn3NY8Hx3DmMR9+a1ISd3OcBcH/5F48pejK1MBtdLOnai64JmI80exT3CR34m3wXpmFbbzQ5jrtGFb63q/n89iVDb+BwY4ctrBn+J7BPEJbhh/aepoUNSG5yICWtjC0q8mDhHzr+40rYsxPXjp9HTaEzgLu+fNhJ0rK+4891Lr08MTud2n8TEntjBRlWQUciGrPn1w3jzIz+q2JdJ35a/MgLg6aRSQOMg6AdanZH2XBTqHbaeYOWrMhmDTjC/Pw9Jczl7S+wr0648bzXz2T AoT_key_test' \
+      'from="10.31.81.5?" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4ohQv1Qksg2sLIqpvjJuZEsIkeLfbPusEaJQerRCqI71g8hwBkED3BBv5FehLcezTg+cFJFhf2vBGV5SbV0NzbouIM+n0lAr6+Ei/XYjO0B1juDm6cUmloD4HSzQWv+cSyNmb7aXjup7V0GP1DZH3zlmvwguhMUTDrWxQxDpoV28m72aZ4qPH7VmQIeN/JG3BF9b9F8P4myOPGuk5XTjY1rVG+1Tm2mxw0L3WuL6w3DsiUrvlXsGE72KcyFBDiFqOHIdnIYWXDLZz61KXctVLPVLMevwU0YyWg70F9pb0d2LZt7Ztp9GxXBRj5WnU9IClaRh58RsYGhPjdfGuoC3P AoT_guest_node_key')
+echo ${keys[@]}
+key_names=('AoT Key' 'AoT Test Key' 'AoT Guest Key')
+for i in $(seq 0 `expr ${#keys[@]} - 1`); do
   key=${keys[i]}
   key_name=${key_names[i]}
-  fgrep $key /home/waggle/.ssh/authorized_keys
+  fgrep "$key" /home/waggle/.ssh/authorized_keys
   print_result "$key_name Auth" $?
 done
 
@@ -32,125 +34,87 @@ print_result "sudo Disabled" $?
 directories=("/etc/waggle" "/usr/lib/waggle" "/usr/lib/waggle/core" "/usr/lib/waggle/plugin_manager" "/usr/lib/waggle/nodecontroller" \
              "/usr/lib/waggle/SSL" "/usr/lib/waggle/SSL/guest" "/usr/lib/waggle/SSL/node" "/usr/lib/waggle/SSL/waggleca")
 for dir in $directories; do
-  print_result "$dir Directory" [ -e $dir ]
+  [ -e $dir ]
+  print_result "$dir Directory" $?
 done
 
 perms=$(stat -c '%U %G %a' /usr/lib/waggle/SSL/guest/id_rsa_waggle_aot_guest_node)
-print_result "Guest Key Permissions" [ $perms == "root root 600" ]
+[ $perms == "root root 600" ]
+print_result "Guest Key Permissions" $?
 
 perms=$(stat -c '%U %G %a' /usr/lib/waggle/SSL/SSL/node/key.pem)
-print_result "Node Key Permissions" [ $perms == "rabbitmq rabbitmq 600" ]
+[ $perms == "rabbitmq rabbitmq 600" ]
+print_result "Node Key Permissions" $?
 
 perms=$(stat -c '%U %G %a' /usr/lib/waggle/SSL/SSL/node/cert.pem)
-print_result "Node Key Permissions" [ $perms == "rabbitmq rabbitmq 600" ]
+[ $perms == "rabbitmq rabbitmq 600" ]
+print_result "Node Key Permissions" $?
 
 perms=$(stat -c '%U %G %a' /usr/lib/waggle/SSL/SSL/waggleca/cacert.pem)
-print_result "Waggle CA Cert Permissions" [ $perms == "root root 644" ]
+[ $perms == "root root 644" ]
+print_result "Waggle CA Cert Permissions" $?
 
 # Ethernet IP Address (NC)
 ifconfig | fgrep "          inet addr:10.31.81.10  Bcast:10.31.81.255  Mask:255.255.255.0"
 print_result "Ethernet IP Address" $?
 
 devices=("waggle_sysmon" "waggle_coresense" "alphasense" "gps_module" "attwwan")
-device_names=(list "WagMan" "Coresense" "Alphasense" "GPS" "Modem")
-for i in $(seq 1 ${#devices[@]}); do
+device_names=("WagMan" "Coresense" "Alphasense" "GPS" "Modem")
+for i in $(seq 0 `expr ${#devices[@]} - 1`); do
   device=${devices[i]}
   device_name=${device_names[i]}
-  print_result "$device_name Device" [ -e $device ]
+  [ -e $device ]
+  print_result "$device_name Device" $?
 done
 
-# Modem USB
-send -- "lsusb | grep --color=never 1bc7:0021\r"
-expect {
-  -exact "ID 1bc7:0021 Telit Wireless Solutions HE910" {
+lsusb | grep 1bc7:0021
+print_result "Modem USB" $?
 
-# Modem IP Address
-send -- "ifconfig | grep ppp0 --color=never -A 1\r"
-expect {
-  -exact "          inet addr:" {
+ifconfig | grep ppp0 -A 1 | fgrep "inet addr:"
+print_result "Modem IP Address" $?
 
-# sshd Listen Addresses (NC)
-send -- "cat /etc/ssh/sshd_config               | grep --color=never -e 'ListenAddress' -e 'PermitRootLogin'\r"
-expect {
-  -exact "ListenAddress 127.0.0.1\r
-ListenAddress 10.31.81.10\r" {
+line_count=$(cat /etc/ssh/sshd_config | fgrep -e 'ListenAddress 127.0.0.1' -e 'ListenAddress 10.31.81.10' | wc -l)
+[ $line_count -eq 2 ]
+print_result "sshd Listen Addresses" $?
 
-# sshd No Root Login (NC)
-  -exact "PermitRootLogin no\r" {
+cat /etc/ssh/sshd_config | fgrep 'PermitRootLogin no'
+print_result "sshd No Root Login" $?
 
-# Node ID Set
-send -- "cat /etc/waggle/node_id | wc -l\r"
-expect {
-  -re "\[1\]" {
+cat /etc/waggle/node_id | egrep '[0-9a-f]{16}'
+print_result "Node ID Set" $?
 
-# Hostname Set (NC)
-  send "cd /usr/lib/waggle/core/scripts\r"
-  send "detect_mac_address.sh\r"
-  send "cd\r"
-} else {
-  send_user "\[0;30;31m\[FAIL\]\[0;30;37m Node ID Set (GN)\n"
-}
+. /usr/lib/waggle/core/scripts/detect_mac_address.sh
+cat /etc/hostname | fgrep "${MAC_STRING}SD"
+print_result "Hostname Set" $?
 
-set success 0
-send -- "cat /etc/hostname                      | grep \${MAC_STRING}SD | wc -l\r"
-expect {
-  -re "\[1\]" {
+parted -s /dev/mmcblk1p2 print | grep --color=never -e ext | awk '{print $3}' | egrep '15\.[0-9]GB'
+print_result "SD Resize" $?
 
-# SD Resize (NC)
-send -- "parted -s /dev/mmcblk1p2 print                     | grep --color=never -e ext\r"
-expect {
-  -re " 1      0\.00B  15\.\[0-9\]GB  15\.\[0-9\]GB  ext" {
+parted -s /dev/mmcblk0p2 print | grep --color=never -e ext | awk '{print $3}' | egrep '15\.[0-9]GB'
+print_result "Recovery to eMMC" $?
 
-# Recovery to eMMC (NC)
-send -- "parted -s /dev/mmcblk0p2 print | grep --color=never -e ext\r"
-expect {
-  -re " 1      0\.00B  15\.\[0-9\]GB  15\.\[0-9\]GB  ext" {
+units=("waggle-communications" "waggle-epoch" "waggle-heartbeat" \
+       "waggle-plugin-manager" "waggle-wagman-publisher" \
+       "waggle-wagman-server" "waggle-wellness")
+for unit in $units; do
+  systemctl status $unit | fgrep 'Active: active (running)'
+  print_result "$unit Service" $?
+done
 
-# waggle-communications Service (NC), etc...
-set units [list "waggle-communications" "waggle-epoch" "waggle-heartbeat" \
-                "waggle-plugin-manager" "waggle-wagman-publisher" \
-                "waggle-wagman-server" "waggle-wellness"]
-foreach unit $units {
-  set success 0
-  send -- "systemctl status ${unit}                  | grep --color=never Active: | sed 's/)..*/)/'\r"
-  expect {
-    -exact "   Active: active (running)" {
+units=("waggle-wwan" "waggle-reverse-tunnel")
+for unit in $units; do
+  systemctl status $unit | fgrep -e 'Active: active (running)' -e 'Active: activating (auto-restart)'
+  print_result "$unit Service" $?
+done
 
-# waggle-wwan Service (NC), etc...
-set units [list "waggle-wwan" "waggle-reverse-tunnel"]
-foreach unit $units {
-  set success 0
-  send -- "systemctl status ${unit}                      | grep --color=never Active: | sed 's/)..*/)/'\r"
-  expect {
-    -exact "   Active: active (running)" {
-      set success 1
-      exp_continue
-    }
-    -exact "   Active: activating (auto-restart)" {
-
-# waggle-log-wagman.timer Service (NC)
-send -- "systemctl status waggle-log-wagman.timer                  | grep --color=never Active: | sed 's/)..*/)/'\r"
-expect {
-  -exact "   Active: active (running)" {
-    set success 1
-    exp_continue
-  }
-  -exact "   Active: active (waiting)" {
-
-# WagMan Device, etc...
-set devices [list "waggle_sysmon" "waggle_coresense" "alphasense" "gps_module" "attwwan"]
-set device_names [list "WagMan" "Coresense" "Alphasense" "GPS" "Modem"]
-
-for {set index 0} {$index < [llength $devices]} {incr index} {
-  set device [lindex $devices $index]
-  set device_name [lindex $device_names $index]
-  set success 0
-  send -- "ls /dev/${device} --color=never\r"
-  expect {
-    "${device}" {
+unit='waggle-log-wagman.timer'
+systemctl status $unit | fgrep -e 'Active: active (running)' -e 'Active: active (waiting)'
+print_result "$unit Service" $?
 
 # ssh to GN
-    send -- "ssh waggle@10.31.81.51                           -o \"StrictHostKeyChecking no\"\r"
+ssh -i /usr/lib/waggle/SSL/guest/id_rsa_waggle_aot_guest_node waggle@10.31.81.51 \
+    -o "StrictHostKeyChecking no" -o "PasswordAuthentication no" -o "ConnectTimeout 2" /bin/date
+print_result "ssh to GN" $?
 
 ##### Repeat above as necessary for GN tests #####
 # Node ID Set (GN)
@@ -174,16 +138,16 @@ for {set index 0} {$index < [llength $devices]} {incr index} {
 ##################################################
 
 # Microphone USB
-send -- "lsusb | grep --color=never 0d8c:013c\r"
-expect {
-  -exact "ID 0d8c:013c C-Media Electronics, Inc. CM108 Audio Controller" {
-
-# Top Camera USB
-send -- "lsusb | grep --color=never 05a3:9830\r"
-expect {
-  -exact "ID 05a3:9830 ARC International" {
-
-# Bottom Camera USB
-send -- "lsusb | grep --color=never 05a3:9520\r"
-expect {
-  -exact "ID 05a3:9520 ARC International" {
+#send -- "lsusb | grep --color=never 0d8c:013c\r"
+#expect {
+#  -exact "ID 0d8c:013c C-Media Electronics, Inc. CM108 Audio Controller" {
+#
+## Top Camera USB
+#send -- "lsusb | grep --color=never 05a3:9830\r"
+#expect {
+#  -exact "ID 05a3:9830 ARC International" {
+#
+## Bottom Camera USB
+#send -- "lsusb | grep --color=never 05a3:9520\r"
+#expect {
+#  -exact "ID 05a3:9520 ARC International" {
