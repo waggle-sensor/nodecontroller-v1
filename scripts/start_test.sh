dac108ea4b67b55682e6535e023d6c084f98ee13
Peter Lane <lane@uchicago.edu>
2016-11-08 16:20:22 -0600

    added a test service start script

---
+++ scripts/start_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ ! -e /home/waggle/continue_test && ! -e /home/waggle/finish_test ]]; then
+  touch /home/waggle/start_test
+fi
+sudo /bin/systemctl start waggle-test.service
+sleep 5
