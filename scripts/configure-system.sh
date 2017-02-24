6dc1dd91b428fd00eb9f3590987f4667f54f4348
Peter Lane <lane@uchicago.edu>
2017-02-24 13:56:35 -0600

    create ssh config for root

--- scripts/configure-system.sh
+++ scripts/configure-system.sh
@@ -38,6 +38,13 @@ echo >> /root/.ssh/authorized_keys
 echo "from=\"10.31.81.5?\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4ohQv1Qksg2sLIqpvjJuZEsIkeLfbPusEaJQerRCqI71g8hwBkED3BBv5FehLcezTg+cFJFhf2vBGV5SbV0NzbouIM+n0lAr6+Ei/XYjO0B1juDm6cUmloD4HSzQWv+cSyNmb7aXjup7V0GP1DZH3zlmvwguhMUTDrWxQxDpoV28m72aZ4qPH7VmQIeN/JG3BF9b9F8P4myOPGuk5XTjY1rVG+1Tm2mxw0L3WuL6w3DsiUrvlXsGE72KcyFBDiFqOHIdnIYWXDLZz61KXctVLPVLMevwU0YyWg70F9pb0d2LZt7Ztp9GxXBRj5WnU9IClaRh58RsYGhPjdfGuoC3P AoT_edge_processor_key" >> /root/.ssh/authorized_keys
 echo >> /root/.ssh/authorized_keys
 
+# add an ssh config host for the Edge Processor
+cat <<EOT > /root/.ssh/config
+Host edgeprocessor
+  StrictHostKeyChecking no
+  ForwardAgent yes
+EOT
+
 # Setup RabbitMQ config files.
 cp -r /usr/lib/waggle/nodecontroller/etc/rabbitmq /etc
 
