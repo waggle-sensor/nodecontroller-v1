f37a8232aa8c6b4afc22446cbe09cdf81d0fdd8d
wcatino <wcatino@gmail.com>
2017-05-17 09:53:24 -0500

    Generalize scripts/autossh.sh to take host and port number from the command line arguments.

--- scripts/autossh.sh
+++ scripts/autossh.sh
@@ -2,9 +2,18 @@
 
 set -x
 
-# TODO! remove these options: -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null"
+if [ $# != 2 ]; then
+    echo "USAGE: " $0 " <HOST>  <PORT>"
+    exit 1
+fi
 
-autossh -i /usr/lib/waggle/SSL/node/key.pem -M 0 -gNC -o "ServerAliveInterval 5" -o "ServerAliveCountMax 3" -R `cat /etc/waggle/reverse_ssh_port`:localhost:22 -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -o "ExitOnForwardFailure yes" root@beehive1.mcs.anl.gov  -p 20022
+HOST=$1
+PORT=$2
+echo 'HOST = ' ${HOST}
+echo 'PORT = ' ${PORT}
+
+# TODO! remove these options: -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null"
+autossh -i /usr/lib/waggle/SSL/node/key.pem -M 0 -gNC -o "ServerAliveInterval 5" -o "ServerAliveCountMax 3" -R ${PORT}:localhost:22 -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -o "ExitOnForwardFailure yes" ${HOST} -p 20022
 
 # For us, autossh should never exit unless there's a problem...however, it's possible that it doesn't
 # exit with an error code needed to have upstart respawn it. This is to correct that possible behavior.
