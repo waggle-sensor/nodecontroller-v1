77faa60ccad4701d5bbc0fd0044e0cfeba65c418
root <park708@purdue.edu>
2017-10-04 19:46:45 +0000

    changes for rabbitmq.config

--- scripts/configure-rabbitmq.sh
+++ scripts/configure-rabbitmq.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 . /usr/lib/waggle/core/scripts/detect_mac_address.sh
-sed -i -e "s/%NODE_ID%/$NODE_ID/" /etc/rabbitmq/rabbitmq.config
+if grep -q "reply_to, <<\"$NODE_ID\">>" "/etc/rabbitmq/rabbitmq.config"; then
+  echo "rabbitmq.config is correct"
+else
+  echo "rabbitmq.config is wrong"
+  echo "copy /usr/lib/waggle/nodecontroller/etc/rabbitmq/rabbitmq.config to /etc/rabbitmq and put node id in the file"
+  cp /usr/lib/waggle/nodecontroller/etc/rabbitmq/rabbitmq.config /etc/rabbitmq
+  sed -i -e "s/%NODE_ID%/$NODE_ID/" /etc/rabbitmq/rabbitmq.config
+fi
