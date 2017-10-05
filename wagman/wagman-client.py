5e37494447697a0ae08e6fd10eb76394f3169fda
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-10-05 15:33:43 -0500

    merge help update

--- wagman/wagman-client.py
+++ wagman/wagman-client.py
@@ -5,31 +5,31 @@ import sys
 import re
 import time
 
-commands = {
-    'rtc',
-    'ping',
-    'start',
-    'stop',
-    'reset',
-    'id',
-    'cu',
-    'hb',
-    'env',
-    'bs',
-    'th',
-    'date',
-    'bf',
-    'fc',
-    'up',
-    'enable',
-    'disable',
-    'watch',
-    'log',
-    'eereset',
-    'boots',
-    'ver',
-    'blf',
-}
+descriptions = sorted([
+    ('rtc', 'shows rtc timestamp'),
+    ('ping', 'simulate a device heartbeat'),
+    ('start', 'starts a device immediately'),
+    ('stop', 'stops a device after specified number of seconds'),
+    ('reset', 'resets the wagman!'),
+    ('id', 'shows wagman id'),
+    ('cu', 'shows current usage'),
+    ('hb', 'shows milliseconds since device heartbeats'),
+    ('env', 'shows wagman temperature and humidity'),
+    ('bs', 'shows selected boot media'),
+    ('th', 'shows thermistor values'),
+    ('date', 'shows current date'),
+    ('bf', 'shows wagman boot flags'),
+    ('fc', 'shows device failure counts'),
+    ('up', 'shows wagman uptime'),
+    ('enable', 'enables device'),
+    ('disable', 'disables device'),
+    ('eereset', 'prepares wagman to persistant memory on next reset'),
+    ('boots', 'shows number of times wagman has booted'),
+    ('ver', 'shows hardware and firmware version info'),
+    ('blf', 'show / set bootloader phase boot flag'),
+])
+
+commands = {name for name, _ in descriptions}
 
 
 def check_args(args):
@@ -90,11 +90,18 @@ def main(args, timeout=15.0, retry_delay=5.0, retry_attempts=3):
 if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser()
+    epilog = 'wagman commands:\n{}'.format('\n'.join(['  {: <10} {}'.format(name, descr) for name, descr in descriptions]))
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='command and controller interface to the wagman.',
+        epilog=epilog)
+
     parser.add_argument('--timeout', type=float, default=15.0)
     parser.add_argument('--retry-delay', type=float, default=5.0)
     parser.add_argument('--retry-attempts', type=int, default=3)
     parser.add_argument('args', nargs='+')
+
     args = parser.parse_args()
 
     main(args=args.args,
