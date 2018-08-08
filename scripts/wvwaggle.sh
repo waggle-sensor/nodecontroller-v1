21d8d81b73ca9e03edf1aa523f2a4efce2f124b5
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-08-08 14:22:56 -0500

    remove device locks

--- scripts/wvwaggle.sh
+++ scripts/wvwaggle.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 #This goes in /usr/bin/wvwaggle.sh, and make the file executable with chmod +x /usr/bin/wvwaggle.sh
 #Let us give the modem time to settle.
-
+rm /var/lock/*attwwan
 rm /dev/attwwan
-sleep 1
+sleep 5
 ln -s $(ls /dev/attwwan[0-9] | sort | head -1) /dev/attwwan
-
-
-
-
+sleep 15
