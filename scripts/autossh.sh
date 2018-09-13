7b4de7f7c52214d9750a0ade6846d351eddaf70f
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-09-12 22:37:17 -0500

    fixed autossh to use new credential location

--- scripts/autossh.sh
+++ scripts/autossh.sh
@@ -5,6 +5,8 @@
 
 set -x
 
+key_file="/etc/waggle/key.pem"
+
 if [ $# == 2 ]; then
     HOST=$1
     PORT=$2
@@ -12,38 +14,26 @@ elif [ $# == 0 ]; then
     HOST="root@beehive"
     PORT=`cat /etc/waggle/reverse_ssh_port`
 else
-    echo "USAGE: " $0 " [<HOST>  <PORT>]"
+    echo "USAGE: $0 [<HOST>  <PORT>]"
     exit 1
 fi
 
-echo 'HOST = ' ${HOST}
-echo 'PORT = ' ${PORT}
+echo "HOST=$HOST"
+echo "PORT=$PORT"
 
-STATUS=0
+if [ -z "$PORT" ]; then
+    echo "Error: Invalid port number \"$PORT\""
+    exit 1
+fi
 
-if [ -z "$PORT" ];then 
-    echo "No PORT number set."
-    STATUS=1
-else if ! [ "$PORT" -eq "$PORT" ] 2> /dev/null
-then
+if ! [ "$PORT" -eq "$PORT" ] 2> /dev/null; then
     echo "Invalid PORT number."
-    STATUS=1
-fi
-fi
-
-if [ ! -f /usr/lib/waggle/SSL/node/key.pem ]; then
-    echo "SSH key file not found."
-    STATUS=1
-fi
-
-
-if [ $STATUS -eq 1 ]; then
-exit 1
+    exit 1
 fi
 
 # TODO! remove these options: -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null"
-autossh -i /usr/lib/waggle/SSL/node/key.pem -M 0 -gNC -o "ServerAliveInterval 5" -o "ServerAliveCountMax 3" -R ${PORT}:localhost:22 -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -o "ExitOnForwardFailure yes" ${HOST} -p 20022
+autossh -i $key_file -M 0 -gNC -o "ServerAliveInterval 5" -o "ServerAliveCountMax 3" -R $PORT:localhost:22 -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -o "ExitOnForwardFailure yes" $HOST -p 20022
 
-# For us, autossh should never exit unless there's a problem...however, it's possible that it doesn't
-# exit with an error code needed to have upstart respawn it. This is to correct that possible behavior.
+# This final exit 1 is to make sure that autossh terminating should be considered
+# an error, even if autossh has return code 0.
 exit 1
