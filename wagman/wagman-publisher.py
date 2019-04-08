2199652752544db07fcad0a6b97afd15bf030409
Vince Forgione <vforgione@uchicago.edu>
2019-04-08 12:44:35 -0500

    Added license notification headers
    I ran the header injection script and a couple manual additions.

--- wagman/wagman-publisher.py
+++ wagman/wagman-publisher.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+# ANL:waggle-license
+#  This file is part of the Waggle Platform.  Please see the file
+#  LICENSE.waggle.txt for the legal details of the copyright and software
+#  license.  For more details on the Waggle project, visit:
+#           http://www.wa8.gl
+# ANL:waggle-license
 """
 The WagMan publisher is responsible for distributing output of the Wagman
 serialline to subscribers. Subscribers may need to use a session ID.
